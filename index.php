00f222bc916f08bf0fb5f4b53abe331e84727b91
heidercan593 <104920009+heidercan593@users.noreply.github.com>
2022-11-01 15:41:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,91 +1,60 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title> Generador de períodos</title>
-<link rel="shortcut icon" href="img/logosena.png">
-<script src="jquery-3.4.1.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  
-<link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/>
-<script src="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.101.0">
+    <title> Generador de períodos</title>
+    <link rel="shortcut icon" href="img/logosena.png">
 
-<link href = "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel = "hoja de estilo" >  
-<script src = "http://code.jquery.com/jquery-2.0.3.min.js" > </script>  
-<script src = "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js" > </script>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/sign-in/">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
 
-<!-- BOOTSTRAP 5 -->
-<link href="  bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
 
-<!-- SWEETALERT2 -->
-<script src="sweetalert2/dist/sweetalert2.all.min.js"></script>
-<link rel="stylesheet" href="sweetalert2/dist/sweetalert2.all.min.js">
-
-<!-- DATATABLE -->
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-<script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-
-<link rel="stylesheet" href="FONDOS.css">
-</head>
-<body>  
-<img src="img/logosena.png" class="rounded float-left" style="width: 130px;" alt="">                      
-<br> 
-<div class="container text-center">
-		<h1 style="margin-top:20px">GENERAR TUS PERÍODOS</h1>
-    <h3>Etapa productiva</h3>
-		<hr>
-
-    <form action="periodos.php" method="post">
-       <div  style="margin-left:350px" class="col-md-4">
-       <div class="input-group mb-3 form-floating">
-       <span class="input-group-text">Fecha de Inicial</span>
-       <input type="date" id="inicio" name="inicio" class="form-control" required><br>
-       </div>
-       <div class="input-group mb-3 form-floating">
-       <span class="input-group-text">Fecha de final</span>
-       <input type="date" id="final" name="final" class="form-control" required><br>
-       </div>
-
-       </div>
-
-       <input type="submit" class="btn btn-outline-success"value="Generar" >                                   
-    </form>
-    <div class="row justify-content-md-center">		
-    <div class="col-md-8">
-    <div id="mostrar_mensaje"></div>
-    </div>
-    </div>                           
-    </div>              
-</body>
-</html>
-
- <script type="text/javascript">
-
-
-function periodos()
-{ 
-  var parametros = 
-  {
-    "bitacora" : $("#bitacora").val(),    
-  };
-
-  $.ajax({
-    data: parametros,
-    url: 'periodos.php',
-    type: 'POST',
     
-    beforesend: function()
-    {
-      $('#mostrar_mensaje').html("Mensaje antes de Enviar");
-    },
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+   
 
-    success: function(mensaje)
-    {
-      $('#mostrar_mensaje').html(mensaje);
-    }
-  });
-}
-    </script>
+<form class="form-signin" action="periodos.php" method="post">
+<h1 class="text-center">Generar períodos de Informes</h1>
+  <img class="mb-4" src="img/logosena.png" alt="" width="100" height="100">
+
+
+
+      <span class="input-group-text" style="color: #3ba241;background-color: #34495e; ">Ingrese fecha Inicio de Etapa Productiva</span>
+       <input type="date" id="inicio" name="inicio" class="form-control" required><br>
+
+       <span class="input-group-text" style="color: #3ba241;background-color: #34495e;" >Ingrese fecha final de Etapa Productiva</span>
+       <input type="date" id="final" name="final" class="form-control" required ><br>
+ 
+       <input type="submit" class="btn btn-outline-success" value="Generar" > 
+  <p class="mt-5 mb-3 text-muted">&copy; Version 1.0</p>
+</form>
+
+
+    
+  </body>
+</html>
